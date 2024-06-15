8f23a54682bb5e033338501a7b158f346cfa77a7
cqx <1062861345@qq.com>
2024-06-15 19:08:35 +0800

    update

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,21 +1,37 @@
 <template>
-  <el-card class="card">
-    <div class="data-card">
-      <div class="png-container">
-        <img src="@/assets/images/people.png" alt="people" class="peoplePng">
-      </div>
-      <div class="text-container">
-        <div class="title-container">
-          <h3 class="title">科研人员</h3>
+  <div class="card-container">
+    <el-card class="card">
+      <div class="data-card">
+        <div class="png-container">
+          <img src="@/assets/images/people.png" alt="people" class="peoplePng">
         </div>
-        <div class="number-container">
-          {{ personnelCount }}
+        <div class="text-container">
+          <div class="title-container">
+            <h3 class="title">科研人员</h3>
+          </div>
+          <div class="number-container">
+            {{ personnelCount }}
+          </div>
         </div>
       </div>
-    </div>
-  </el-card>
+    </el-card>
 
-
+    <el-card class="card">
+      <div class="data-card">
+        <div class="png-container">
+          <img src="@/assets/images/meeting.png" alt="people" class="peoplePng">
+        </div>
+        <div class="text-container">
+          <div class="title-container">
+            <h3 class="title">会议</h3>
+          </div>
+          <div class="number-container">
+            {{ meetingCount }}
+          </div>
+        </div>
+      </div>
+    </el-card>
+  </div>
 
   <el-card>
     <el-row :gutter="20" class="header">
@@ -50,6 +66,7 @@ import { Search } from '@element-plus/icons-vue'
 import { ref } from 'vue'
 
 const personnelCount = ref(102400); // 假设这是从变量得到的数字
+const meetingCount = ref(81212);
 
 const queryForm = ref({
   pagenum: 1,
@@ -149,10 +166,17 @@ const handleSearch = () => {
   margin-top:10px;
 }
 
-.card {
-  width:20%;
-  margin-bottom: 10px;
+.card-container {
+  display: flex;
+  justify-content: flex-start;
+  .card {
+    width:20%;
+    margin-bottom: 10px;
+    margin-left: 5px;
+    margin-right: 5px;
+  }
 }
+
 .data-card {
   width: 90%;
   display: flex;
@@ -160,6 +184,7 @@ const handleSearch = () => {
 
   .png-container {
     width: 30%;
+    margin-left: 10%;
 
     .peoplePng {
       width: 80%;
@@ -169,7 +194,12 @@ const handleSearch = () => {
 
   .text-container {
     width: 30%;
+    margin-right: 5%
   }
+}
+
+.title-container {
+  margin-bottom: 5px;
 }
 
 </style>
